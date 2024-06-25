f179c5adc5c2ce740bb3361b0826bdd16f79877c
PRAISOODAN <praisoodanvk@gmail.com>
2024-06-24 23:25:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var sidenav =  document.querySelector (".side-navbar")
+
+
+function closeNavbar() {
+   sidenav.style.left ="-60%";  
+}
+
+function showNavbar() {
+    sidenav.style.left ="0%";  
+ }
